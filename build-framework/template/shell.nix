a7808417ce08914bfb71ab8a46fbc7ed5df2299b
hughjfchen <hugh.jf.chen@gmail.com>
2020-12-09 10:27:08 +0800

    use hlint 2.2.11 for now because 3.0 is too new

--- build-framework/template/shell.nix
+++ build-framework/template/shell.nix
@@ -15,7 +15,8 @@
 
     # Some common tools can be added with the `tools` argument
     #tools = { cabal = "3.2.0.0"; hlint = "2.2.11"; };
-    tools = { cabal = "MY_CABAL_VER"; hlint = "MY_HLINT_VER"; };
+    #tools = { cabal = "MY_CABAL_VER"; hlint = "MY_HLINT_VER"; };
+    tools = { cabal = "MY_CABAL_VER"; hlint = "2.2.11"; };
     # See overlays/tools.nix for more details
 
     # Some you may need to get some other way.
