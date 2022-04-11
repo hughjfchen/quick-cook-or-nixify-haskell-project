40d2c097fbbc243ecf647839776fc4e790879c48
hughjfchen <hugh.jf.chen@gmail.com>
2022-04-11 16:35:35 +0800

    finish nixify.sh

--- nixify.sh
+++ nixify.sh
@@ -14,7 +14,7 @@ usage_and_exit () {
 
 init_with_root_or_sudo "$0"
 
-SCRIPT_ABS_PATH=$(turn_to_absolute_path $0)
+SCRIPT_ABS_PATH=$(turn_to_absolute_path "$0")
 
 if [ "$#" != 3 ]; then 
 	usage_and_exit
@@ -24,27 +24,72 @@ begin_banner "Top level" "project nixifying"
 
 "${SCRIPT_ABS_PATH}"/prepare-env/do.sh
 
+# make a temp dir
+quick_cook_nixify_tmp_dir=$(mktemp -d -t quick-cook-nixify-"$(date +%Y%m%d%H%M%S)"-XXXXXXXXXX)
+
 case $3 in
   generate)
-         "${SCRIPT_ABS_PATH}"/project-scaffold-generate/do.sh "$1" "$2"
+         echo "nixify only support template"
 
-         "${SCRIPT_ABS_PATH}"/build-framework/do.sh "$1" "$2"
-
-	# copy the common and prepare-env for CI for the generated project
-	# because it may need to setup env within a CI environment
-	mkdir -p "$1/$2/ci"
-	cp -R "${SCRIPT_ABS_PATH}"/common "$1/$2/ci/"
-	cp -R "${SCRIPT_ABS_PATH}"/prepare-env "$1/$2/ci/"
           ;;
   template)
-         "${SCRIPT_ABS_PATH}"/project-scaffold-template/do.sh "$1" "$2"
+         "${SCRIPT_ABS_PATH}"/project-scaffold-template/do.sh "${quick_cook_nixify_tmp_dir}" "$2"
 
-         "${SCRIPT_ABS_PATH}"/fix-up/do.sh "$1" "$2"
+         "${SCRIPT_ABS_PATH}"/fix-up/do.sh "${quick_cook_nixify_tmp_dir}" "$2"
+
+         [ -d "$1/$2/ci" ] && mv "$1/$2/ci" "$1/$2/ci.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp -R "${quick_cook_nixify_tmp_dir}/$2/ci" "$1/$2"
+         [ -d "$1/$2/nix" ] && mv "$1/$2/nix" "$1/$2/nix.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp -R "${quick_cook_nixify_tmp_dir}/$2/nix" "$1/$2"
+         [ -f "$1/$2/default.nix" ] && mv "$1/$2/default.nix" "$1/$2/default.nix.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/default.nix" "$1/$2"
+         [ -f "$1/$2/shell.nix" ] && mv "$1/$2/shell.nix" "$1/$2/shell.nix.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/shell.nix" "$1/$2"
+         [ -f "$1/$2/cross-build.nix" ] && mv "$1/$2/cross-build.nix" "$1/$2/cross-build.nix.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/cross-build.nix" "$1/$2"
+         [ -f "$1/$2/docker.nix" ] && mv "$1/$2/docker.nix" "$1/$2/docker.nix.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/docker.nix" "$1/$2"
+         [ -f "$1/$2/tarball.nix" ] && mv "$1/$2/tarball.nix" "$1/$2/tarball.nix.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/tarball.nix" "$1/$2"
+         [ -f "$1/$2/hie.yaml" ] && mv "$1/$2/hie.yaml" "$1/$2/hie.yaml.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/hie.yaml" "$1/$2"
+         [ -f "$1/$2/fourmolu.yaml" ] && mv "$1/$2/fourmolu.yaml" "$1/$2/fourmolu.yaml.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/fourmolu.yaml" "$1/$2"
+         [ ! -f "$1/$2/cabal.project.local" ] && cp "${quick_cook_nixify_tmp_dir}/$2/cabal.project.local" "$1/$2"
+         if [ -f "$1/$2/cabal.project" ]; then
+             THE_INDEX_STATE=$(grep index-state "${quick_cook_nixify_tmp_dir}/$2/cabal.project")
+             grep -v "index-state" "$1/$2/cabal.project" > "${quick_cook_nixify_tmp_dir}/$2/cabal.project.orig"
+             echo "${THE_INDEX_STATE}" >> "${quick_cook_nixify_tmp_dir}/$2/cabal.project.orig"
+             mv "$1/$2/cabal.project" "$1/$2/cabal.project.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+             cp "${quick_cook_nixify_tmp_dir}/$2/cabal.project.orig" "$1/$2/cabal.project"
+         else
+             cp "${quick_cook_nixify_tmp_dir}/$2/cabal.project" "$1/$2"
+         fi
+         [ -f "$1/$2/.gitignore" ] && mv "$1/$2/.gitignore" "$1/$2/.gitignore.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/.gitignore" "$1/$2"
+         [ -f "$1/$2/.dir-locals.el" ] && mv "$1/$2/.dir-locals.el" "$1/$2/.dir-locals.el.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/.dir-locals.el" "$1/$2"
+         [ -f "$1/$2/.ghci" ] && mv "$1/$2/.ghci" "$1/$2/.ghci.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/.ghci" "$1/$2"
+         [ -f "$1/$2/.hlint.yaml" ] && mv "$1/$2/.hlint.yaml" "$1/$2/.hlint.yaml.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/.hlint.yaml" "$1/$2"
+         [ -f "$1/$2/start-dev" ] && mv "$1/$2/start-dev" "$1/$2/start-dev.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/start-dev" "$1/$2"
+         mkdir -p "$1/$2/.github/workflows"
+         [ -f "$1/$2/.github/workflows/nix-ci.yml" ] && mv "$1/$2/.github/workflows/nix-ci.yml" "$1/$2/.github/workflows/nix-ci.yml.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         cp "${quick_cook_nixify_tmp_dir}/$2/.github/workflows/nix-ci.yml" "$1/$2/.github/workflows/"
+
+         if [ -f "$1/$2/$2.cabal" ] && [ -f "$1/$2/stack.yaml" ]; then
+             mv "$1/$2/stack.yaml" "$1/$2/stack.yaml.bak.by.nixify.$(date +%Y%m%d%H%M%S)"
+         fi
 	 ;;
  *)
 	 ;;
 esac
 
+
+# clean up
+rm -fr "${quick_cook_nixify_tmp_dir}"
 
 # everything's done
 done_banner "Top level" "project nixifying"
