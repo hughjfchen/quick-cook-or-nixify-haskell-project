7561a724f39b2fcc126dcb5b7ffa88eeba976b70
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-04-21 22:56:43 -0500

    add interpreter to the scripts.

--- project-scaffold-template/nixos2111-ghc8107-haskell.nix/ci/pack-tarball/undo.sh
+++ project-scaffold-template/nixos2111-ghc8107-haskell.nix/ci/pack-tarball/undo.sh
@@ -1,18 +1,15 @@
-#!/usr/bin/env bash
 
 if ! type dirname > /dev/null 2>&1; then
     echo "Not even a linux or macOS, Windoze? We don't support it. Abort."
     exit 1
 fi
 
-. "$(dirname "$0")"/../common/common.sh
-
 init_with_root_or_sudo "$0"
 
-begin_banner "Top level" "project deploy - packing"
+SCRIPT_ABS_PATH=$(turn_to_absolute_path $0)
 
-set +u
-[[ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]] && . $HOME/.nix-profile/etc/profile.d/nix.sh
-set -u
+begin_banner "Top level" "project deploy - unpacking"
 
-done_banner "Top level" "project deploy - packing"
+tar zPxf "${SCRIPT_ABS_PATH}/{{name|toSnake}}.tar.gz
+
+done_banner "Top level" "project deploy - unpacking"
