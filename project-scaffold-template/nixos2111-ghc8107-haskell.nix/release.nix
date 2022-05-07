fe3404fff412570c34948070506211cf662b3fe5
hughjfchen <hugh.jf.chen@gmail.com>
2022-05-07 17:59:12 +0800

    add some extra runtime dependences for reference.

--- project-scaffold-template/nixos2111-ghc8107-haskell.nix/release.nix
+++ project-scaffold-template/nixos2111-ghc8107-haskell.nix/release.nix
@@ -11,6 +11,67 @@ let
   nPkgs = nativePkgs.pkgs;
   sPkgs = pkgs.x86-musl64; # for the fully static build
   lib = nPkgs.lib; # lib functions from the native package set
+
+  # extra runtime dependencies
+  # eclipse package and plugins
+  my-eclipse-mat = nPkgs.eclipse-mat.overrideAttrs (oldAttrs: { buildCommand = nPkgs.lib.concatStringsSep "\n" [
+                                            oldAttrs.buildCommand
+                                            "mv $out/mat/MemoryAnalyzer $out/mat/eclipse"
+                                            "mv $out/mat/MemoryAnalyzer.ini $out/mat/eclipse.ini"
+                                            "mv $out/mat $out/eclipse"
+                                            "sed \'s/\\/mat\\/MemoryAnalyzer/\\/eclipse\\/eclipse/g\' $out/bin/eclipse-mat > $out/bin/eclipse"
+                                            "chmod +x $out/bin/eclipse"
+                                            ];});
+  mat-with-dtfj = nPkgs.eclipses.eclipseWithPlugins {
+    eclipse = my-eclipse-mat;
+    jvmArgs = [ "-Xmx4096m" ];
+    plugins = [
+      (nPkgs.eclipses.plugins.buildEclipsePlugin {
+        name = "ibm-dtfj";
+        srcFeature = builtins.fetchurl {
+          url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/runtimes/tools/dtfj/features/com.ibm.dtfj.feature_1.12.29003.202006111057.jar";
+          sha256 = "1jfmb96qn422wrqkkbmd8n0lgdpx0c2g2lbhas00j8020w29yiw8";
+        };
+        srcPlugins = [
+          (builtins.fetchurl {
+            url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/runtimes/tools/dtfj/plugins/com.ibm.dtfj.api_1.12.29003.202006111057.jar";
+            sha256 = "0qcmhdh2skbjqmfi42sq4i7zfr2arkvna0qb3k4ci1d36c21d4y1";
+          })
+          (builtins.fetchurl {
+            url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/runtimes/tools/dtfj/plugins/com.ibm.dtfj.j9_1.12.29003.202006111057.jar";
+            sha256 = "1v2vs3xwngsqsvy2vhajqm3i42dx8j99yacir1sp7xbicx5cdiy6";
+          })
+        ];
+      })
+    ];
+  };
+
+  # java jar packages
+  my-jca = nPkgs.stdenv.mkDerivation {
+    name = "my-jca";
+    version = "4611";
+    src = builtins.fetchurl {
+      url = "https://public.dhe.ibm.com/software/websphere/appserv/support/tools/jca/jca4611.jar";
+      sha256 = "16wrbxl229qr4bnmdpdi1swmgfgy8irq35gmbcicgaq3grga781q";
+    };
+    dontBuild = true;
+    dontUnpack = true;
+    unpackPhase = "";
+    installPhase = ''
+      mkdir -p $out/share/java
+      cp $src $out/share/java/
+    '';
+  };
+  my-jca-wrap = nPkgs.symlinkJoin {
+    name = "my-jca-wrap";
+    paths = [ nPkgs.xvfb-run my-jca ];
+    buildInputs = [ nPkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/xvfb-run --add-flags "-a java -jar $out/share/java/jca${my-jca.version}.jar" --prefix PATH : $out/bin --prefix LANG : en_US.utf-8
+    '';
+  };
+
+  # the config
   {{name}}-config = nPkgs.writeTextFile {
     name = "{{name}}-config";
     # generate the key = value format config, refer to the lib.generators for other formats
@@ -18,7 +79,9 @@ let
   };
   {{name}}-bin-wrap-paths = [
     # list the runtime dependencies, especially those cannot be determined by nix automatically
+    nPkgs.jdk
     nPkgs.xvfb-run
+    nPkgs.my-jca-wrap
     sPkgs.{{name}}.{{name}}-exe
   ];
   {{name}}-bin-wrap = nPkgs.symlinkJoin {
