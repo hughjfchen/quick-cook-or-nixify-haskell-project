0ecf3fd63b162b748f122d2a772ef0c1d0c6acff
hughjfchen <hugh.jf.chen@gmail.com>
2022-05-15 11:31:52 +0800

    remove disable systemd service when fixup undo

--- project-scaffold-template/nixos2111-ghc8107-haskell.nix/cd/fix-up/undo.sh
+++ project-scaffold-template/nixos2111-ghc8107-haskell.nix/cd/fix-up/undo.sh
@@ -17,7 +17,8 @@ set -u
 
 if [ -n "$RELEASE_HAS_SYSTEMD_SERVICE" ]; then
     THE_SERVICE_NAME=$(awk -F'"' '/unitsToStart\+\=/ {print $2}' "$NIX_STORE_PATH"/bin/setup-systemd-units)
-    [ -n "$THE_SERVICE_NAME" ] && sudo systemctl stop "$THE_SERVICE_NAME" && sudo systemctl disable "$THE_SERVICE_NAME"
+    # [ -n "$THE_SERVICE_NAME" ] && sudo systemctl stop "$THE_SERVICE_NAME" && sudo systemctl disable "$THE_SERVICE_NAME"
+    [ -n "$THE_SERVICE_NAME" ] && sudo systemctl stop "$THE_SERVICE_NAME"
 fi
 
 done_banner "Top level" "project deploy - fixup - undo"
